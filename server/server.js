dcf011e4f0deb6a6faecbdcafc1b4f18e09ac6b8
kmirijan <34608342+kmirijan@users.noreply.github.com>
2018-05-08 11:33:21 -0700

    test Delete

--- server/server.js
+++ server/server.js
@@ -40,7 +40,7 @@ app.get('/todos/:id', (req, res) => {
 
 	Todo.findById(id).then((todo) => {
 		if(!todo){
-			res.status(404).send();
+			return res.status(404).send();
 		}
 
 		res.send({todo});
@@ -57,11 +57,12 @@ app.delete('/todos/:id', (req, res) => {
 
 	Todo.findByIdAndRemove(id).then((todo) => {
 		if(!todo){
-			res.status(404).send();
+			return res.status(404).send();
 		}
 
-		res.status(200).send(todo);
-	}).catch((e) => res.status(400).send());
+		res.status(200).send({todo});
+	}).catch((e) =>
+		res.status(400).send());
 });
 
 app.listen(port, () => {
